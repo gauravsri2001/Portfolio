c9f4221d4c51f865a50333dc4d1bb507a27baacf
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-12-07 23:44:08 +0530

    ..

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,7 +20,21 @@ const About = () => {
 
             </span>
             <br/>
-            
+
+            <div className='w-[14rem]'>
+                <a 
+                href={Resume}
+                download="Resume"
+                target='blank'
+                rel="noreferrer">
+
+                <div className='p-4 mt-2 w-[13rem] rounded-xl text-orange-500 flex justify-center border-2 border-orange-500 hover:border-violet-500 hover:shadow-pink-500'> Resume
+
+                </div>
+
+                </a>
+
+            </div>
         </div>
 
     </div>
