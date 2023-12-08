1bb71e4cd57fab9ce03c46d5e944b9d94ba0e068
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-12-09 01:39:51 +0530

    ..

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@
 import './App.css';
 import About from './Components/About';
 import Main from './Components/Main';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      {/* <Main /> */}
-      <About />
+    
+
+    <div className='flex flex-col overflow-x-hidden'>
+      
+      <div className='absolute w-screen'>
+        <Navbar />
+      </div>
+      
+      <div className='min-h-screen'>
+        <div className='absolute'>
+          <Main />
+
+        </div>
+        <div className='relative'>
+          <About />
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
