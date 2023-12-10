df8b3e4da2da2e8253494b91e7c27b379fe3a44c
Gaurav Srivastava <2001srigaurav@gmail.com>
2023-12-10 16:47:10 +0530

    ..

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {GrLinkedinOption, GrMail} from 'react-icons/gr'
+import { FaGithub } from "react-icons/fa6";
 import { BiPhone } from 'react-icons/bi';
 const Footer = () => {
   return (
     <div>
-        <div className='w-[screen] bg-black'>
-            <div className='flex flex-row p-2 flex-wrap justify-around'>
+        <div className='w-[screen] p-5 items-center justify-center bg-black  flex flex-row'>
+
+            <div className=' flex flex-row p-2 mr-2 flex-wrap justify-around'>
                 <a href='www.linkedin.com/in/gaurav-srivastava2001' target='blank' className='text-lg flex flex-row text-violet-300'>
                     <div className='h-[30px] w-[30px] rounded-full flex justify-center items-center bg-violet-400'>
                         <GrLinkedinOption className="text-white"></GrLinkedinOption>
@@ -13,7 +15,18 @@ const Footer = () => {
                 </a>
             </div>
 
-            <div className='flex flex-row p-2 flex-wrap justify-around'>
+            <div className='flex flex-row p-2 mr-2 flex-wrap justify-around'>
+                <a href='https://github.com/gauravsri2001' target='blank' className='text-lg flex flex-row text-violet-300'>
+                    <div className='h-[30px] w-[30px] rounded-full flex justify-center items-center bg-violet-400'>
+                        
+                        <FaGithub className=" text-white" />
+                    </div>
+                </a>
+            </div>
+
+            
+
+            <div className='flex flex-row p-2 mr-2 flex-wrap justify-around'>
                 <a href='2001srigaurav@gmail.com' target='blank' className='text-lg flex flex-row text-violet-300'>
                     <div className='h-[30px] w-[30px] rounded-full flex justify-center items-center bg-violet-400'>
                         <GrMail className="text-white"></GrMail>
@@ -22,7 +35,7 @@ const Footer = () => {
                 </a>
             </div>
 
-            <div className='flex flex-row p-2 flex-wrap justify-around'>
+            <div className='flex flex-row p-2 mr-2 flex-wrap justify-around'>
                 <a href='//' target='blank' className='text-lg flex flex-row text-violet-300'>
                     <div className='h-[30px] w-[30px] rounded-full flex justify-center items-center bg-violet-400'>
                         <BiPhone className='text-white ml-1 mt-[0.1rem]' />
